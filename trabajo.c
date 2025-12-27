c8a12ac222a1d35562ffcd2f5e121eb2c4f90515
Val2a <dar.mar.gar2000@gmail.com>
2025-12-27 17:21:09 +0100

    feat: file readed properly

--- trabajo.c
+++ trabajo.c
@@ -2,17 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
-    int pid, prn;
-    MPI_Offset offset;
-    MPI_File fh;
-
-    // Variables para guardar argumentos
+    // Variables
+    // 1. Argumentos
     int k;
     char *dataPath;
     int numThreads;
+    // 2. MPI
+    int pid, prn;
+    // 3. Archivo
+    MPI_Offset offset;
+    MPI_File fh;
+    int rows, cols;
+    // 4. Programa
+    int splitSize, restSize;
+    double *matrix;
+    double *buffer;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &pid);
@@ -22,33 +30,50 @@ int main(int argc, char *argv[])
     {
         if (pid == 0)
         {
-            printf("Error: Los argumentos son: -np <Num. procesos> <Num. K de vecinos> <Ruta a datos> <Num. hilos>\n");
+            printf("Error: Los argumentos son: -np <Num. procesos> nombrePrograma <Num. K de vecinos> <Ruta a datos> <Num. hilos>\n");
         }
         MPI_Finalize();
         return 0;
     }
 
-    if (pid == 0)
-    {
-        // Ponemos los argumentos en su sitio
-        k = atoi(argv[1]);
-        dataPath = argv[2];
-        numThreads = atoi(argv[3]);
-    }
+    k = atoi(argv[1]);
+    dataPath = argv[2];
+    numThreads = atoi(argv[3]);
+    printf("Datapath: %s\n", dataPath);
 
-    // Mandamos los argumentos a todos
-    // podríamos calcularlos por separado ya que no es muy costoso,
-    // pero creo que queda más elegante así.
-    MPI_Bcast(k, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(dataPath, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(numThreads, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    printf("[PID: %d]: k:%d\n", pid, k);
+
+    omp_set_num_threads(numThreads);
 
     // Abrimos el archivo (todos lo hacen)
     MPI_File_open(MPI_COMM_WORLD, dataPath, MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
 
-    offset = pid * sizeof(double) * 24;
+    if (pid == 0)
+    {
+        // Obtenemos el número de filas y columnas
+        // Cada uno de esos dos números ocupa exactamente
+        // 32 bits en memoria, por eso usamos éste tipo
+        // en específico.
+        MPI_File_read(fh, &rows, 1, MPI_INT32_T, 0);
+        MPI_File_read(fh, &cols, 1, MPI_INT32_T, 0);
+    }
 
-    MPI_File_read_at_all(fh, offset, lineBuffer, columns, MPI_DOUBLE, 0);
+    MPI_Bcast(&rows, 1, MPI_INT32_T, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&cols, 1, MPI_INT32_T, 0, MPI_COMM_WORLD);
+
+    // Numero de lineas para cada proceso
+    splitSize = rows / prn;
+    // Lineas restantes
+    restSize = rows % prn;
+
+    offset = 8 + pid * splitSize * cols * sizeof(double);
+
+    buffer = (double *)malloc(splitSize * cols * sizeof(double));
+
+    MPI_File_read_at_all(fh, offset, buffer, splitSize * cols, MPI_DOUBLE, 0);
+
+    printf("[PID: %d] Offset: %d, primer double: %0.2f\n", pid, offset, buffer[0]);
+    free(buffer);
     MPI_Finalize();
     return 0;
 }
