3bbb82d11d0d64a1e173012baa50aaf35243f3b3
Val2a <dar.mar.gar2000@gmail.com>
2025-12-27 11:25:09 +0100

    feat: shallow argument check done

--- trabajo.c
+++ trabajo.c
@@ -5,4 +5,22 @@
 
 int main(int argc, char *argv[])
 {
+    int pid, prn;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+    MPI_Comm_size(MPI_COMM_WORLD, &prn);
+
+    if (argc < 4)
+    {
+        if (pid == 0)
+        {
+            printf("Error: Los argumentos son: <Num. K de vecinos> <Ruta a datos> <Num. procesos> <Num. hilos>\n");
+        }
+        MPI_Finalize();
+        return 0;
+    }
+
+    MPI_Finalize();
+    return 0;
 }
