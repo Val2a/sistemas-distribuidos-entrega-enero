5ba6c742b2a53380f0c826729197bd6a6cdba850
Val2a <dar.mar.gar2000@gmail.com>
2025-12-29 14:06:29 +0100

    feat: a function has been created to handle the search of a row

--- trabajo.c
+++ trabajo.c
@@ -22,6 +22,11 @@ int checkArguments(int pid, int argc);
  */
 float calculateMAPE(float *vPredict, float *vReal, int size);
 
+/**
+ * Busca una línea (wantedRow) y la guarda en *rowDataBuffer, en
+ */
+void searchRow(int pid, int prn, int wantedRow, int splitRows, int cols, float *localM, float *restM, float *rowDataBuffer, int *pidBuffer);
+
 int main(int argc, char *argv[])
 {
     // Variables
@@ -160,8 +165,14 @@ int main(int argc, char *argv[])
                 targetRowData[j] = localMatrix[rowToSearchIndex * cols + j];
             }
         }
+        searchRow(pid, prn, rowToSearch, splitRows, cols, localMatrix, restMatrix, targetRowData, &pidWithTheRow);
 
-        MPI_Bcast(targetRowData, cols, MPI_FLOAT, pidWithTheRow, MPI_COMM_WORLD);
+        if (pidWithTheRow == pid)
+        {
+            printf("[PID: %d] Linea encontrada: Primer float: %0.1f, ultimo float: %0.1f\n", pid, targetRowData[0], targetRowData[cols - 1]);
+        }
+
+        // MPI_Bcast(targetRowData, cols, MPI_FLOAT, pidWithTheRow, MPI_COMM_WORLD);
     }
 
     // Liberamos la memoria asignada
@@ -204,26 +215,26 @@ float calculateMAPE(float *vPredict, float *vReal, int size)
     return (sum * 100.0f) / (float)size;
 }
 
-void searchRow(int pid, int prn, int rowToSearch, int splitRows, int cols, float *localM, float *restM, float *rowBuffer)
+void searchRow(int pid, int prn, int wantedRow, int splitRows, int cols, float *localM, float *restM, float *rowDataBuffer, int *pidBuffer)
 {
-    int pidWithTheRow = rowToSearch / splitRows;
-    int rowToSearchIndex = rowToSearch % splitRows;
+    pidBuffer = wantedRow / splitRows;
+    int rowToSearchIndex = wantedRow % splitRows;
     float *m = localM;
 
     // El pid no puede ser igual ni mayor que prn.
     // Si se da aquí el caso, significa que la línea
     // a buscar está en las lineas de resto.
-    if (pidWithTheRow == prn)
+    if (pidBuffer == prn)
     {
-        pidWithTheRow = 0;
+        pidBuffer = 0;
         m = restM;
     }
 
-    if (pidWithTheRow == pid)
+    if (pidBuffer == pid)
     {
         for (int j = 0; j < cols; j++)
         {
-            rowBuffer[j] = m[rowToSearchIndex * cols + j];
+            rowDataBuffer[j] = m[rowToSearchIndex * cols + j];
         }
     }
 }
